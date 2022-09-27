f47fb8b1147e10740315e749b44cd60f453ebce6
Yeeun55 <joice9559@gmail.com>
2022-09-27 22:03:06 +0900

    feat(Report): fix service & add testcode

--- src/main/java/bungae/thunder/cakey/service/ReportService.java
+++ src/main/java/bungae/thunder/cakey/service/ReportService.java
@@ -7,33 +7,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReportService {
     private final ReportRepository reportRepository;
 
     @Autowired
-    public ReportService(ReportRepository reportRepository){
+    public ReportService(ReportRepository reportRepository) {
         this.reportRepository = reportRepository;
     }
 
     /**
      * message 신고
      */
-    public Long makeReport(Report report, Message message, String content){
+    public Long makeReport(Report report, Message message, String content) {
         report.setMessageId(message.getId());
         report.setContents(content);
 
         reportRepository.save(report);
         return report.getId();
+    }
 
+    public Optional<Report> getReport(Long id){
+        return reportRepository.findById(id);
     }
 
     /**
      * 특정 메세지 신고 내역 조회
      */
-    public List<Report> getAllReports(Long messageId){
-        return reportRepository.findAllByMessageId(messageId);
+    public List<Report> getAllReportsByMessage(Message message) {
+        return reportRepository.findAllByMessageId(message.getId());
     }
 
 
--- src/test/java/bungae/thunder/cakey/repository/MemoryReportRepositoryTest.java
+++ src/test/java/bungae/thunder/cakey/repository/MemoryReportRepositoryTest.java
@@ -4,6 +4,8 @@ import bungae.thunder.cakey.domain.Report;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class MemoryReportRepositoryTest {
@@ -15,7 +17,7 @@ public class MemoryReportRepositoryTest {
     }
 
     @Test
-    public void save(){
+    public void save() {
         // given
         Report report = new Report();
         report.setMessageId(111L);
@@ -25,7 +27,88 @@ public class MemoryReportRepositoryTest {
         reportRepository.save(report);
 
         //then
+
         Report result = reportRepository.findById(report.getId()).get();
         assertThat(result).isEqualTo(result);
     }
+
+    @Test
+    public void findAllByMessageId() {
+        // given
+        Report report1 = new Report();
+        report1.setMessageId(111L);
+        report1.setContents("this is VERY bad");
+        reportRepository.save(report1);
+
+        Report report2 = new Report();
+        report2.setMessageId(222L);
+        report2.setContents("this is VERYVERY bad");
+        reportRepository.save(report2);
+
+
+        Report report3 = new Report();
+        report3.setMessageId(111L);
+        report3.setContents("this is VERYVERYVERY bad");
+        reportRepository.save(report3);
+
+
+        Report report4 = new Report();
+        report4.setMessageId(11L);
+        report4.setContents("this is bad. you could DIE");
+        reportRepository.save(report4);
+
+        Report report5 = new Report();
+        report5.setMessageId(11L);
+        report5.setContents("NOPE");
+        reportRepository.save(report5);
+
+        // when
+        List<Report> result1 = reportRepository.findAllByMessageId(111L);
+        List<Report> result2 = reportRepository.findAllByMessageId(11L);
+        List<Report> result3 = reportRepository.findAllByMessageId(222L);
+
+
+        // then
+        assertThat(result1.size()).isEqualTo(2);
+        assertThat(result2.size()).isEqualTo(2);
+        assertThat(result3.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void findAll() {
+        // given
+        Report report1 = new Report();
+        report1.setMessageId(111L);
+        report1.setContents("this is VERY bad");
+        reportRepository.save(report1);
+
+        Report report2 = new Report();
+        report2.setMessageId(222L);
+        report2.setContents("this is VERYVERY bad");
+        reportRepository.save(report2);
+
+
+        Report report3 = new Report();
+        report3.setMessageId(111L);
+        report3.setContents("this is VERYVERYVERY bad");
+        reportRepository.save(report3);
+
+
+        Report report4 = new Report();
+        report4.setMessageId(11L);
+        report4.setContents("this is bad. you could DIE");
+        reportRepository.save(report4);
+
+        Report report5 = new Report();
+        report5.setMessageId(11L);
+        report5.setContents("NOPE");
+        reportRepository.save(report5);
+
+        List<Report> result = reportRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(5);
+    }
+
 }
