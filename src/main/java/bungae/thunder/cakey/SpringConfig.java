79ca9ff0aa62797cbdcf1f0f6c4a0f86b7c95740
Yeeun55 <joice9559@gmail.com>
2022-10-15 16:55:00 +0900

    feat(Report): spring config

--- src/main/java/bungae/thunder/cakey/SpringConfig.java
+++ src/main/java/bungae/thunder/cakey/SpringConfig.java
@@ -1,9 +1,10 @@
 package bungae.thunder.cakey;
 
-import bungae.thunder.cakey.repository.MemoryUserRepository;
-import bungae.thunder.cakey.repository.UserRepository;
+import bungae.thunder.cakey.repository.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
 
 @Configuration
 public class SpringConfig {
@@ -12,4 +13,11 @@ public class SpringConfig {
         // 추후에 해당 부분은 PostgresqlUserRepository로 교체
         return new MemoryUserRepository();
     }
+
+    @Bean
+    public ReportRepository reportRepository() {
+        return new MemoryReportRepository();
+    }
+
 }
+
