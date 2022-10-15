3b9fd83dd8750de77b41f6aec42d7c6b38a61c26
jinwoo.baek <jinwoo.baek@linecorp.com>
2022-10-15 16:35:41 +0900

    add getusers in contorller

--- src/main/java/bungae/thunder/cakey/controller/UserController.java
+++ src/main/java/bungae/thunder/cakey/controller/UserController.java
@@ -1,13 +1,26 @@
 package bungae.thunder.cakey.controller;
 
+import bungae.thunder.cakey.domain.User;
 import bungae.thunder.cakey.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Slf4j
+@Controller
 public class UserController {
     private final UserService userService;
 
     @Autowired // 자동으로 스프링이 userService를 연결
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.findMembers();
     }
 }
