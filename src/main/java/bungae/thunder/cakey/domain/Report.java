9e19fcf8811e80c1d5db80acc2d4f209589493d3
Yeeun55 <joice9559@gmail.com>
2022-12-03 15:01:14 +0900

    fix: post 오류 수정

--- src/main/java/bungae/thunder/cakey/domain/Report.java
+++ src/main/java/bungae/thunder/cakey/domain/Report.java
@@ -2,9 +2,11 @@ package bungae.thunder.cakey.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
-@Builder
+@Getter
+@NoArgsConstructor
 public class Report {
     /**
      * TODO: 논의 내용
@@ -15,5 +17,16 @@ public class Report {
     private Long id;
     private Long messageId;
     private String contents;
+
+    @Builder
+    public Report(Long id, Long messageId, String contents) {
+        this.id = id;
+        this.messageId = messageId;
+        this.contents = contents;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
