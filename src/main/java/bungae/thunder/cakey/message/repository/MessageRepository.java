c02ef5f963773eba532627e4d07d065ca31b205b
jinwoo.baek <jinwoo.baek@linecorp.com>
2022-12-07 22:51:53 +0900

    fix(total): remove unnecesaary import

--- src/main/java/bungae/thunder/cakey/message/repository/MessageRepository.java
+++ src/main/java/bungae/thunder/cakey/message/repository/MessageRepository.java
@@ -4,7 +4,6 @@ import bungae.thunder.cakey.message.domain.Message;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MessageRepository {
--- src/main/java/bungae/thunder/cakey/user/repository/UserRepository.java
+++ src/main/java/bungae/thunder/cakey/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package bungae.thunder.cakey.user.repository;
 import bungae.thunder.cakey.user.domain.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository {
     User save(User user);
