7c7f9395f430ba0b014880db4fd2c8cbe6be2749
gyurinida <gyurinida@naver.com>
2023-02-11 16:01:47 +0900

    mariadb & jpa

--- src/main/java/bungae/thunder/cakey/cake/domain/Cake.java
+++ src/main/java/bungae/thunder/cakey/cake/domain/Cake.java
@@ -1,12 +1,20 @@
 package bungae.thunder.cakey.cake.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cake {
 
-    private Long id;
+    @Id @GeneratedValue private Long id;
+
     private Integer year;
     private Long userId;
 
