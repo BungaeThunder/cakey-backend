5fdf75367ea437e4d795faf3d4b8a6da137f01cd
gyurinida <gyurinida@naver.com>
2023-03-04 17:06:04 +0900

    #48 [Cake] JPA 적용

--- src/main/java/bungae/thunder/cakey/SpringConfig.java
+++ src/main/java/bungae/thunder/cakey/SpringConfig.java
@@ -1,12 +1,9 @@
 package bungae.thunder.cakey;
 
-import bungae.thunder.cakey.cake.repository.CakeRepository;
-import bungae.thunder.cakey.cake.repository.MemoryCakeRepository;
 import bungae.thunder.cakey.message.repository.MemoryMessageRepository;
 import bungae.thunder.cakey.message.repository.MessageRepository;
 import bungae.thunder.cakey.report.repository.MemoryReportRepository;
 import bungae.thunder.cakey.report.repository.ReportRepository;
-
 import java.util.HashMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories
 public class SpringConfig {
-    @Bean
-    public CakeRepository cakeRepository() {
-        return new MemoryCakeRepository();
-    }
-
     @Bean
     public MessageRepository messageRepository() {
         // TODO: DB Repository
