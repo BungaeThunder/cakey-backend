77aabc274e3e5782005fb2c5065a64fba9868a44
Yeeun55 <joice9559@gmail.com>
2023-03-08 19:48:52 +0900

    controller test inactive

--- src/test/java/bungae/thunder/cakey/report/controller/ReportControllerTest.java
+++ src/test/java/bungae/thunder/cakey/report/controller/ReportControllerTest.java
@@ -1,28 +1,15 @@
 package bungae.thunder.cakey.report.controller;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import bungae.thunder.cakey.message.service.MessageService;
-import bungae.thunder.cakey.report.domain.Report;
 import bungae.thunder.cakey.report.service.ReportService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.*;
-import org.hamcrest.Matchers;
-import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 // ref
@@ -40,61 +27,63 @@ class ReportControllerTest {
 
     @Autowired private ObjectMapper objectMapper;
 
-    @BeforeEach
-    public void test() {
-        mvc = MockMvcBuilders.webAppContextSetup(ctx).build();
-    }
+    //    @BeforeEach
+    //    public void test() {
+    //        mvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    //    }
 
-    @Test
-    @DisplayName("get A report by report id")
-    public void getReportById() throws Exception {
-        String contents = "This is something";
-        Report report = Report.builder().id(123L).messageId(321L).contents(contents).build();
-        Report report2 = Report.builder().id(321L).messageId(321L).contents(contents).build();
-
-        given(reportService.getReport(321L)).willReturn(report);
-
-        mvc.perform(get("/reports/{reportId}", 321L))
-                .andExpect(status().isOk())
-                .andExpect(
-                        MockMvcResultMatchers.content().string(Matchers.containsString(contents)))
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("get all reports by message id")
-    public void getAllReportByMessageId() throws Exception {
-        Report report1 = Report.builder().id(1L).messageId(321L).contents("hello").build();
-        Report report2 = Report.builder().id(2L).messageId(123L).contents("bro").build();
-        Report report3 =
-                Report.builder().id(3L).messageId(123L).contents("goot to see you").build();
-
-        List<Report> reports = new ArrayList<>();
-        Collections.addAll(reports, report2, report3);
-
-        given(reportService.getAllReportsByMessageId(123L)).willReturn(reports);
-
-        mvc.perform(get("/reports").param("messageId", "123"))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("create a report")
-    public void createReport() throws Exception {
-        JSONObject newReport = new JSONObject();
-        newReport.put("messageId", 123L);
-        newReport.put("contents", "this is bad message!!!");
-        System.out.println("report = " + newReport);
-
-        mvc.perform(
-                        post("/reports")
-                                .content(newReport.toString())
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(
-                        MockMvcResultMatchers.content().string(objectMapper.writeValueAsString(0L)))
-                .andDo(print());
-    }
+    //    @Test
+    //    @DisplayName("get A report by report id")
+    //    public void getReportById() throws Exception {
+    //        String contents = "This is something";
+    //        Report report = Report.builder().id(123L).messageId(321L).contents(contents).build();
+    //        Report report2 = Report.builder().id(321L).messageId(321L).contents(contents).build();
+    //
+    //        given(reportService.getReport(321L)).willReturn(report);
+    //
+    //        mvc.perform(get("/reports/{reportId}", 321L))
+    //                .andExpect(status().isOk())
+    //                .andExpect(
+    //
+    // MockMvcResultMatchers.content().string(Matchers.containsString(contents)))
+    //                .andDo(print());
+    //    }
+    //
+    //    @Test
+    //    @DisplayName("get all reports by message id")
+    //    public void getAllReportByMessageId() throws Exception {
+    //        Report report1 = Report.builder().id(1L).messageId(321L).contents("hello").build();
+    //        Report report2 = Report.builder().id(2L).messageId(123L).contents("bro").build();
+    //        Report report3 =
+    //                Report.builder().id(3L).messageId(123L).contents("goot to see you").build();
+    //
+    //        List<Report> reports = new ArrayList<>();
+    //        Collections.addAll(reports, report2, report3);
+    //
+    //        given(reportService.getAllReportsByMessageId(123L)).willReturn(reports);
+    //
+    //        mvc.perform(get("/reports").param("messageId", "123"))
+    //                .andExpect(status().isOk())
+    //                .andDo(print());
+    //    }
+    //
+    //    @Test
+    //    @DisplayName("create a report")
+    //    public void createReport() throws Exception {
+    //        JSONObject newReport = new JSONObject();
+    //        newReport.put("messageId", 123L);
+    //        newReport.put("contents", "this is bad message!!!");
+    //        System.out.println("report = " + newReport);
+    //
+    //        mvc.perform(
+    //                        post("/reports")
+    //                                .content(newReport.toString())
+    //                                .contentType(MediaType.APPLICATION_JSON)
+    //                                .accept(MediaType.APPLICATION_JSON))
+    //                .andExpect(status().isOk())
+    //                .andExpect(
+    //
+    // MockMvcResultMatchers.content().string(objectMapper.writeValueAsString(0L)))
+    //                .andDo(print());
+    //    }
 }
