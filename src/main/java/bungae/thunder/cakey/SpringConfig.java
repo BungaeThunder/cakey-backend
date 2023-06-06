dd232b6fdeb6d2003da8ff607a46277953d1a674
jinwoo.baek <jinwoo.baek@linecorp.com>
2023-06-06 16:51:32 +0900

    apply cors

--- src/main/java/bungae/thunder/cakey/SpringConfig.java
+++ src/main/java/bungae/thunder/cakey/SpringConfig.java
@@ -1,8 +1,25 @@
 package bungae.thunder.cakey;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaRepositories
-public class SpringConfig {}
+public class SpringConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
