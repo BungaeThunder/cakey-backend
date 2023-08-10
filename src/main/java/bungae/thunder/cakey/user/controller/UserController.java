59ed0fc0aa143ae197d72a0e669a835791351d33
jinwoo.baek <jinwoo.baek@linecorp.com>
2023-08-10 19:07:51 +0900

    add userDetail response & fix user/{userid} api

--- src/main/java/bungae/thunder/cakey/user/controller/UserController.java
+++ src/main/java/bungae/thunder/cakey/user/controller/UserController.java
@@ -1,7 +1,12 @@
 package bungae.thunder.cakey.user.controller;
 
+import bungae.thunder.cakey.cake.domain.Cake;
+import bungae.thunder.cakey.cake.service.CakeService;
+import bungae.thunder.cakey.letter.domain.Letter;
+import bungae.thunder.cakey.letter.service.LetterService;
 import bungae.thunder.cakey.user.converter.UserResponseDtoConverter;
 import bungae.thunder.cakey.user.domain.User;
+import bungae.thunder.cakey.user.dto.UserDetailResponse;
 import bungae.thunder.cakey.user.dto.UserResponseDto;
 import bungae.thunder.cakey.user.dto.UserSignUpRequestDto;
 import bungae.thunder.cakey.user.service.UserService;
@@ -16,14 +21,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserService userService;
+    private UserService userService;
+    private CakeService cakeService;
+    private LetterService letterService;
 
     private UserResponseDtoConverter UserResponseDtoConverter;
 
     @Autowired
     public UserController(
-            UserService userService, UserResponseDtoConverter UserResponseDtoConverter) {
+            UserService userService,
+            CakeService cakeService,
+            LetterService letterService,
+            UserResponseDtoConverter UserResponseDtoConverter) {
         this.userService = userService;
+        this.cakeService = cakeService;
+        this.letterService = letterService;
         this.UserResponseDtoConverter = UserResponseDtoConverter;
     }
 
@@ -38,8 +50,14 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId) {
-        return ResponseEntity.ok(UserResponseDtoConverter.convert(userService.getUser(userId)));
+    public ResponseEntity<UserDetailResponse> getUser(@PathVariable Long userId) {
+        User user = userService.getUser(userId);
+        List<Cake> cakes = cakeService.getAllCakes(userId);
+        List<Letter> letters = letterService.getAllLettersBySenderId(userId);
+        UserDetailResponse response = new UserDetailResponse(user, cakes, letters);
+        System.out.printf("cakes : %s", cakes);
+        System.out.printf("response : %s", response);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/signUp")
---
+++ src/main/java/bungae/thunder/cakey/user/dto/UserDetailResponse.java
@@ -0,0 +1,48 @@
+package bungae.thunder.cakey.user.dto;
+
+import bungae.thunder.cakey.cake.domain.Cake;
+import bungae.thunder.cakey.letter.domain.Letter;
+import bungae.thunder.cakey.user.domain.User;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
+
+@Getter
+public class UserDetailResponse {
+    private Long userId;
+    private String email;
+    private String name;
+    private LocalDate birthday;
+    private List<CakeResponse> cakes;
+    private List<LetterResponse> letters;
+
+    public UserDetailResponse(User user, List<Cake> cakes, List<Letter> letters) {
+        this.userId = user.getId();
+        this.email = user.getEmail();
+        this.name = user.getName();
+        this.birthday = user.getBirthday();
+        this.cakes = cakes.stream().map(CakeResponse::new).collect(Collectors.toList());
+        this.letters = letters.stream().map(LetterResponse::new).collect(Collectors.toList());
+    }
+
+    @Getter
+    public static class CakeResponse {
+        private Long id;
+        private Integer year;
+
+        public CakeResponse(Cake cake) {
+            this.id = cake.getId();
+            this.year = cake.getYear();
+        }
+    }
+
+    @Getter
+    public static class LetterResponse {
+        private Long id;
+
+        public LetterResponse(Letter letter) {
+            this.id = letter.getId();
+        }
+    }
+}
