049aafebc2c0f301dd1f82a83ab91cfe9b9be94c
Hakjun Lee <haakjunlee@gmail.com>
2023-09-30 14:43:03 +0900

    chore: 설정 고침

--- src/main/java/bungae/thunder/cakey/auth/OAuth2LoginSecurityConfig.java
+++ src/main/java/bungae/thunder/cakey/auth/OAuth2LoginSecurityConfig.java
@@ -20,7 +20,7 @@ public class OAuth2LoginSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest()
-                .permitAll()
+                .authenticated()
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint()
